6dd99b44e3f8761681422a60b0d895469cbf8c4e
ningliu <ningliu@sf-express.com>
2018-12-28 21:01:08 +0800

    update scancode

--- server/controllers/addbook.js
+++ server/controllers/addbook.js
@@ -1,11 +1,30 @@
 const https = require('https')
+const { mysql } = require('../qcloud')
 
 module.exports = async (ctx, next) => {
     const { isbn, openid } = ctx.request.body
     if (isbn && openid) {
         const url = `https://api.douban.com/v2/book/isbn/${isbn}`
         const bookinfo = await getJSON(url)
-        console.log(bookinfo)
+        const { title, image, alt, publisher, summary, price, rating: { average: rate } } = bookinfo
+        const tags = bookinfo.tags.map(item => `${item.title} ${item.count}`).join(',')
+        const author = bookinfo.author.join(',')
+        const book = { isbn, openid, title, image, alt, publisher, summary, price, rate, tags, author }
+        try {
+            await mysql('books').insert(book)
+            ctx.state.data = {
+                title,
+                msg: 'success'
+            }
+        } catch (e) {
+            ctx.state = {
+                code: -1,
+                data: {
+                    msg: `添加图书失败${e.sqlMessage}`
+                }
+            }
+        }
+        console.log(book.publisher)
     }
 }
 
