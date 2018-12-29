9c039fe325908715a561803bcfa99567534ccd48
ningliu <ningliu@sf-express.com>
2018-12-29 15:10:56 +0800

    update booklist

--- server/routes/index.js
+++ server/routes/index.js
@@ -34,4 +34,6 @@ router.post('/message', controllers.message.post)
 
 router.post('/addbook', controllers.addbook)
 
+router.get('/booklist', controllers.booklist)
+
 module.exports = router
--- src/pages/books/index.vue
+++ src/pages/books/index.vue
@@ -1,8 +1,29 @@
 <template>
-  <p>图书列表</p>
+  <div>
+    <p>图书列表</p>
+    <div v-for="book in books" :key="book.id">{{book.title}}</div>
+  </div>
 </template>
 
 <script>
+import { get } from '@/util';
+
+export default {
+  data() {
+    return {
+      books: []
+    };
+  },
+  methods: {
+    async getList() {
+      const books = await get('/weapp/booklist');
+      this.books = books.list;
+    }
+  },
+  mounted() {
+    this.getList();
+  }
+};
 </script>
 <style>
 </style>
